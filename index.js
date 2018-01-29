72975b837cb857027955c2720a6c04c25a50cebc
Suraj Nath <devversitymail@gmail.com>
2018-01-29 21:05:36 +0530

    fill in the in out hour and desc text area

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
+
+/**
+ * Get a random integer between `min` and `max`.
+ * 
+ * @param {number} min - min number
+ * @param {number} max - max number
+ * @return {number} a random integer
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 async function run() {
   const browser = await puppeteer.launch({
     headless: false,
@@ -54,8 +66,25 @@ async function run() {
   await page.waitFor(2*1000);
   await page.goto('https://nucleus.niituniversity.in/WebApp/StudParentDashBoard/DailyDiary.aspx', {waitUntil: 'networkidle2'});
   // await page.click('#ctl00_ContentPlaceHolder1_btnDDiary');
-  await page.screenshot({ path: 'screenshots/nu.png' });
   // await browser.close();
+  in_hour = ("0" + getRandomInt(8, 10)).slice(-2);
+  in_min = ("0" + getRandomInt(1, 59)).slice(-2);
+  out_hour = ("0" + getRandomInt(19, 22)).slice(-2);
+  out_min = ("0" + getRandomInt(1, 59)).slice(-2);
+  console.log(in_hour)
+  console.log(in_min)
+  console.log(out_hour)
+  console.log(out_min)
+  // fill in the in and out hours
+  await page.select('select[name="ctl00$ContentPlaceHolder1$Timeinhr"]', in_hour);
+  await page.select('select[name="ctl00$ContentPlaceHolder1$Timeinmin"]', in_min);
+  await page.select('select[name="ctl00$ContentPlaceHolder1$Timeouthr"]', out_hour);
+  await page.select('select[name="ctl00$ContentPlaceHolder1$Timeoutmin"]', out_min);
+  // fill in the desc.
+  await page.type('#txtDesc', 'Hello World', {delay: 100}); // Types slower, like a user
+
+  await page.screenshot({ path: 'screenshots/nu.png' });
+
 }
 
 run();
