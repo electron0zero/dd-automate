955c655e09690c29296981b509072d251c05731f
Suraj Nath <devversitymail@gmail.com>
2018-01-31 01:06:29 +0530

    email + error handling + docs
    - email for failure and sucess
    - document stuff

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # dd-automate
 Automate my Daily Diary
 
-### How to provide creds
+### How to setup
+
+I am using SenGrid for email see [@sendgrid/mail](https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail)
+
+Get your SendGrid API key from [SendGrid](https://app.sendgrid.com/settings/api_keys) and pass them as env. variable
+
+Now create creds in root of project
+
 1. make a file `creds.js`
 2. copy paste following js in it
 ```js
 module.exports = {
-    username: '<username>',
-    password: '<password>'
+    gEmail: '<username>',
+    gPass: '<password>',
+    SENDGRID_API_KEY: '<your-api-key>'
 }
 ```
-3. Now use those in your code.
-4. Have Fun and Enjoy
+3. change <username> and <password> with your university gmail account creds.
+4. change <your-api-key> with sendgrid api key
+
+### How to run
+`node index.js`
+
+### Refer these for setup and other things
+http://www.spacjer.com/blog/2014/02/10/defining-node-dot-js-task-for-heroku-scheduler/
+https://github.com/rspacjer/notyfication-sender
+https://sendgrid.com/docs/Classroom/Troubleshooting/Account_Administration/heroku_users_find_your_sendgrid_credentials.html
+
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var time = require('time');
 const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
+const sgMail = require('@sendgrid/mail');
 
-var sendgrid  = require('sendgrid')(
-  process.env.SENDGRID_USERNAME,
-  process.env.SENDGRID_PASSWORD
-);
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+sgMail.setApiKey(CREDS.SENDGRID_API_KEY);
 
 // Text for weekend
 weekend_text = "Weekend - Leave at Office"
 
-// texts for weekdays
-// total 9
+// texts for weekdays - total 9
 week_texts = [
 "Fixed a bug and made a PR",
 "Merged a feature I was working on",
@@ -23,102 +22,150 @@ week_texts = [
 "Discussed new feature and worked on it",
 "Worked on onging feature"]
 
-message_for_email = "dd-automate email \n\n\n"
+// message for email we send when we reach at the end of run function
+message_for_email = "<h2>dd-automate email</h2><br>"
 
-/**
- * Get a random integer between `min` and `max`.
- * returned number is inclusive of min and max
- */
+// catch everything uncaught and email me when this happens
+// consider this poor man's monitoring
+process.on('uncaughtException', function (err) {
+  now = new Date().toString()
+  message = "<h2>Something Happend in dd-automate which was unexpacted</h2>"
+  message = message + "<p>At Time: " + now + "</p><br>"
+  message = message + "<p><strong>Error log</strong></p>"
+  message = message + "<pre>" + err.stack + "</pre>"
+  console.log(message)
+  sendEmail("Error in dd-automate - uncaughtException", message)
+});
+
+// ========================
+// Helper Functions
+// ========================
+
+// Get a random integer between 'min' and 'max' 
+// returned number is inclusive of min and max
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-// for (i = 0; i < 1000; i++ ){
-//   console.log(getRandomInt(1, 10))
-// }
-
 // Tells if it's weekend in India (IST timezone)
 function isWeekendInIST() {
   t = new time.Date();
   t.setTimezone("Asia/Calcutta");
-  message_for_email = message_for_email + "Filling Daily Diary at: " + t.toString() + "\n"
+  message_for_email = message_for_email + "<p>Filling Daily Diary at: " + t.toString() + "</p>"
   // console.log(t.toString())
   day = t.getDay()
   // 6 = Saturday, 0 = Sunday
   isWeekend = (day == 6) || (day == 0);
   return isWeekend
 }
-
+// get desc text to fill in, it takes care of weekend and weekdays
 function getText() {
   // in JS function is true, so without () it will go in true always
   if (isWeekendInIST()) {
     return weekend_text
   } 
   else{
-    return week_texts[getRandomInt(0,8)]
+    return week_texts[getRandomInt(0,(week_texts.length - 1))]
   }
 }
+// build email message object
+function buildMessage(subject, message){
+  const msg = {
+    to: 'devversitymail@gmail.com',
+    from: 'no-reply@dd-automate.com',
+    subject: subject,
+    text: message,
+    html: message
+  };
+  return msg  
+}
+// send email
+function sendEmail(subject, message){
+  sgMail.send(buildMessage(subject, message));
+}
 
+// =====================================
+// async run() function
+// =====================================
+
+// this function is resposnible for all puppetier things
 async function run() {
+  // config browser
   const browser = await puppeteer.launch({
     headless: false,
   });
   // Log Into University Google Account
   const pageg = await browser.newPage();
   await pageg.goto('https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin', {waitUntil: 'networkidle2'});
-  // dom element selectors
+  // DOM element selectors
   const USERNAME_SELECTOR = '#identifierId';
   const PASSWORD_SELECTOR = '#password';
   await pageg.click(USERNAME_SELECTOR);
-  await pageg.keyboard.type(CREDS.gu);
+  await pageg.keyboard.type(CREDS.gEmail);
   await pageg.keyboard.press('Tab');  
   await pageg.keyboard.press('Tab');
   await pageg.keyboard.press('Enter');
-  // await pageg.click(PASSWORD_SELECTOR);
-  await pageg.waitFor(1000)
-  await pageg.keyboard.type(CREDS.gp);
+  await pageg.waitFor(10000)
+  await pageg.keyboard.type(CREDS.gPass);
   await pageg.keyboard.press('Tab');
-  await pageg.keyboard.press('Enter');  
+  await pageg.keyboard.press('Enter');
 
+  // In a new page log into NU ERP
   const page = await browser.newPage();
+
+  // handle popup messages
   page.on('dialog', async dialog => {
-    message_for_email = message_for_email + "Message in Popup: " + dialog.message() + "\n"
-    // console.log(dialog.message());
+    message_for_email = message_for_email + "<p>Message in Popup: " + dialog.message() + "</p>"
     dialog.accept(); // click ok on alert
   });
   // log into NU ERP
-  await page.waitFor(5*1000);
+  // wait for good amount of time to ensure that page gets loaded
+  await page.waitFor(10*1000);
   await page.goto('https://nucleus.niituniversity.in/', {waitUntil: 'networkidle2'});
   await page.click('#lnklogingoogle', {waitUntil: 'networkidle2'});
-  await page.waitFor(5*1000);
+  await page.waitFor(10*1000);
   await page.goto('https://nucleus.niituniversity.in/WebApp/StudParentDashBoard/DailyDiary.aspx', {waitUntil: 'networkidle2'});
-  // await page.click('#ctl00_ContentPlaceHolder1_btnDDiary');
-  // await browser.close();
+
+  // generate random in and out time in ranges
   in_hour = ("0" + getRandomInt(8, 10)).slice(-2);
   in_min = ("0" + getRandomInt(1, 59)).slice(-2);
   out_hour = ("0" + getRandomInt(19, 22)).slice(-2);
   out_min = ("0" + getRandomInt(1, 59)).slice(-2);
-  message_for_email = message_for_email + "In Time: " + in_hour + ":" + in_min + "\n" 
-  message_for_email = message_for_email + "out Time: " + out_hour + ":" + out_min + "\n"
-  // console.log(in_hour)
-  // console.log(in_min)
-  // console.log(out_hour)
-  // console.log(out_min)
+  message_for_email = message_for_email + "<p>In Time: " + in_hour + ":" + in_min + "</p>" 
+  message_for_email = message_for_email + "<p>Out Time: " + out_hour + ":" + out_min + "</p>"
+
   // fill in the in and out hours
   await page.select('select[name="ctl00$ContentPlaceHolder1$Timeinhr"]', in_hour);
   await page.select('select[name="ctl00$ContentPlaceHolder1$Timeinmin"]', in_min);
   await page.select('select[name="ctl00$ContentPlaceHolder1$Timeouthr"]', out_hour);
   await page.select('select[name="ctl00$ContentPlaceHolder1$Timeoutmin"]', out_min);
+
   // fill in the desc.
   text_for_desc = getText();
-  message_for_email = message_for_email + "Desc: " + text_for_desc + "\n"
+  message_for_email = message_for_email + "<p>Desc: " + text_for_desc + "</p>"
   await page.type('#txtDesc', text_for_desc, {delay: 10}); // Types slower, like a user
+
   // click the submit buttom
   await page.click('#ctl00_ContentPlaceHolder1_btnSubmit', {waitUntil: 'networkidle2'});
-  // log email message
-  console.log(message_for_email)
+
+  // send Email
+  sendEmail("dd-automate - Daily Diary Email", message_for_email)
 }
 
-// console.log(isWeekendInIST());
-// console.log(getText())
-run();
+// ================================
+// main execution point, run() will
+// run the whole routine
+// ================================
+
+// catch error an email me
+// another bit of poor man's monitoring
+try {
+  run();
+} catch (err) {
+  now = new Date().toString()
+  message = "<h2>run() failed in dd-automate</h2>"
+  message = message + "<p>At Time: " + now + "</p><br>"
+  message = message + "<p><strong>Error log</strong></p>"
+  message = message + "<pre>" + err.stack + "</pre>"
+  console.log(message)
+  sendEmail("Error in dd-automate - run() Failed", message)
+}
